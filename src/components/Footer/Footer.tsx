f9fe88a2a7fd4b3b012ce3cb4ff88bbcbc0e80da
Shirly Manor <shirly.manor@gmail.com>
2023-10-25 12:38:35 -0700

    Update Footer.tsx
    Add widget

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,6 +7,25 @@ const Footer = () => {
         <p className="text-center text-slate-400">
           Built by <a className="underline font-medium text-inherit" href="https://twitter.com/colbyfayock">Colby Fayock</a>
         </p>
+        <p>
+        <button id="upload_widget" class="cloudinary-button">Upload files</button>
+
+<script src="https://upload-widget.cloudinary.com/global/all.js" type="text/javascript"></script>  
+
+<script type="text/javascript">  
+var myWidget = cloudinary.createUploadWidget({
+  cloudName: 'shirly', 
+  uploadPreset: 'my-preset'}, (error, result) => { 
+    if (!error && result && result.event === "success") { 
+      console.log('Done! Here is the image info: ', result.info); 
+    }
+  }
+)
+
+document.getElementById("upload_widget").addEventListener("click", function(){
+    myWidget.open();
+  }, false);
+</script></p>
       </Container>
     </footer>
   );
